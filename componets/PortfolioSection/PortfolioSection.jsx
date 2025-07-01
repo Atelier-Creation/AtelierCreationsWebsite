3ff42129eeb8e411e03f2d0f5a5a27fb53b7fe97
JagadishRamakrishnan <jeevasumathi1357@gmail.com>
2025-07-01 11:38:45 +0530

    updated responsive

--- componets/PortfolioSection/PortfolioSection.jsx
+++ componets/PortfolioSection/PortfolioSection.jsx
@@ -291,7 +291,7 @@ const PortfolioSection = () => {
       scrollTrigger: {
         trigger: ".card-wrapper",
         start: "top 30%",
-        end: "bottom+=350%",
+        end: "bottom+=450%",
         scrub: 1,
         pin: true,
         anticipatePin: 0,
